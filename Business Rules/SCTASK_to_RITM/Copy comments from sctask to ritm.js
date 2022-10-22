f1443372fa75a9d680c0431d0ef6efd8f2b4cb50
Abhishekchowdarapu <60147157+Abhishekchowdarapu@users.noreply.github.com>
2022-10-23 00:04:59 +0530

    Copy latest comments from SCTASK  to RITM
    This Business rule copies latest comment from SCTASK to RITM. The existing code doesn't get comments. This is a fix to SCTASK to RITM

---
+++ Business Rules/SCTASK_to_RITM/Copy comments from sctask to ritm.js
@@ -0,0 +1,13 @@
+/*
+Table: sc_task
+When to Run: after update
+Condition: current.comments.changes() && gs.isInteractive()
+*/
+
+//Script to update comments on RITM.
+
+var ritmGr = new GlideRecord('sc_req_item');
+if(ritmGr.get(current.request_item.sys_id)){
+
+ritmGr.comments=current.comments.getJournalEntry(1);// This gets the latest comment added.
+}
